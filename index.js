028700bfa733e0684517230bf3023339accde298
Muzaffar <abbossth@gmail.com>
2025-10-24 17:25:23 +0500

    telegram bot yaratildi

---
+++ index.js
@@ -0,0 +1,10 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "7884966007:AAEDoJw5reFpTSk4o1qx3xzum3cBTrMnuiM"
+
+const bot = new TelegramBot(TOKEN, { polling: true })
+
+bot.on("message", function (msg) {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Salom" )
+})
